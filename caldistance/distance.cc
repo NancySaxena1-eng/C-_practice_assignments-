8ad576ef7d9085311931155052f0b9e1da3675b4
Nancy Saxena <84478790+NancySaxena1-eng@users.noreply.github.com>
2022-10-06 10:35:43 -0700

    Add files via upload

---
+++ caldistance/distance.cc
@@ -0,0 +1,45 @@
+/* Name :Nancy Saxena , SID :014554891 */
+
+#include"distance.h"
+#include<iostream>
+#include<cmath>
+/*Overloading  for two points */
+
+float distance(XYPoint p1, XYPoint p2){
+        //calculating distance between p1 and p2
+  	float z = sqrt((p2.x - p1.x) * (p2.x - p1.x)
+		       +(p2.y - p1.y) * (p2.y - p1.y));
+ 	return z;
+
+}
+float distance(XYPoint p1, XYPoint p2, XYPoint p3){
+	//calculating distance between p1 and p2
+  	float a = distance(p1, p2);
+	//calculate distance between p3 and p2
+  	float b = distance(p2, p3);
+	//adding the distances
+	float z = a + b;
+	return z;
+
+
+}
+float distance(XYPoint p1, XYPoint p2, XYPoint p3, XYPoint p4){
+	//calculate the distance between p1 to p3 
+  	float a = distance(p1, p2, p3);
+	//calculate the distance between p3 and p4
+  	float b = distance(p3, p4);
+	//adding all the distances
+	float z = a + b;
+	return z;
+
+}
+float distance(XYPoint p1, XYPoint p2, XYPoint p3, XYPoint p4, XYPoint p5){
+	//calculate the distance between p1 to p4
+  	float a = distance(p1, p2, p3, p4);
+	//calculate the distance between p4 and p5
+  	float b = distance(p4, p5);
+	//adding all the distances
+	float z = a + b;
+	return z;
+
+}
---
+++ caldistance/distance.h
@@ -0,0 +1,11 @@
+/*Name :Nancy Saxena ,SID ;014554891 */
+#ifndef distance_h
+#define distance_h
+#include "XYPoint.h"
+
+float distance(XYPoint p1,XYPoint p2);
+float distance(XYPoint p1,XYPoint p2, XYPoint p3);
+float distance(XYPoint p1,XYPoint p2, XYPoint p3,XYPoint p4);
+float distance(XYPoint p1,XYPoint p2, XYPoint p3,XYPoint p4,XYPoint p5);
+
+#endif
