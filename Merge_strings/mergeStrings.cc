91a7bd8f82c6b9d73429d1d1ecd376f5746d4c61
Nancy Saxena <84478790+NancySaxena1-eng@users.noreply.github.com>
2022-10-06 10:46:26 -0700

    Add files via upload

---
+++ Merge_strings/mergeStrings.cc
@@ -0,0 +1,50 @@
+/* Name :Nancy Saxena SID:014554891 */
+#include <iostream>
+#include <string>
+#include <cstring>
+using std::string;
+string mergeStrings(const string &a , const string &b){
+	int c = a.length();
+	int d = b.length();
+	string x;
+	int remain = 0;
+	bool inLoop = false;
+	/* Checking for the length of the string is equal */
+	if (c == d) {
+		for (int i = 0 ; i < c ; i++) {
+			x += a.at(i);
+			x += b.at(i);
+		}
+	}
+	/* Checking if the length of the string a is greater than string b */
+	else if (c > d) {
+		for (int i = 0; i < d; i++) {
+			x += a.at(i);
+			x += b.at(i);
+			remain = i;
+			inLoop = true;
+
+		}
+		if (!inLoop) {
+			x = x + a;
+		} else {
+			x = x + a.substr(remain + 1, c);
+		}
+
+	}
+	/* Checking if the length of the string b is greater than string a */
+	else {
+		for (int i = 0; i < c; i++){
+			x += a.at(i);
+			x += b.at(i);
+			remain = i;
+			inLoop = true;
+		}
+		if (!inLoop) {
+			x = x + b;
+		} else {
+			x = x + b.substr(remain + 1, d);
+		}
+	}
+	return x;
+}
---
+++ Merge_strings/mergeStrings.h
@@ -0,0 +1,7 @@
+/*Name :Nancy Saxena SID:014554891 */
+#ifndef MERGESTRING_H
+#define MERGESTRING_H
+#include <string>
+using std::string;
+string mergeStrings(const string &a , const string &b);
+#endif 
